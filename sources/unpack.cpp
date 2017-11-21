cdc37ff3cb02366f3fb5d3d794d4f3c34b007c65
ovan-iu8 <32965256+ovan-iu8@users.noreply.github.com>
2017-11-22 01:56:26 +0300

    Add files via upload

---
+++ sources/unpack.cpp
@@ -0,0 +1,25 @@
+#include "json.hpp"
+#include <fstream>
+#include <iomanip> // for std::setw
+
+using json = nlohmann::json;
+
+int main(int argc, char const *argv[]) {
+	try {
+		if(argc == 1)
+			throw std::runtime_error("ERROR: have no any path...");
+		
+		// read a JSON file
+		std::ifstream in( argv[1] );
+		json _j;
+		in >> _j;
+		in.close();
+
+		std::cout << std::setw(4) << _j << std::endl;
+
+	} catch(const std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+	return 0;
+}
